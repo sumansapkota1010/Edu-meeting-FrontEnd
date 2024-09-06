5faf130f8f0a76a5510a38be4e5ef9d791a4cb24
suman <sumansapkota777@gmail.com>
2024-09-06 13:16:58 +0545

    feat:HomeFixed

--- src/components/Main/ContactUs/ContactUs.jsx
+++ src/components/Main/ContactUs/ContactUs.jsx
@@ -56,9 +56,9 @@ const ContactUs = () => {
             style={{ backgroundImage: `url(${contactBg})` }}
         >
             <div className="container mx-auto">
-                <div className="flex flex-wrap justify-between">
-                    <div className="w-full lg:w-9/12">
-                        <div className="bg-white rounded-2xl p-10">
+                <div className="flex flex-wrap justify-between lg:h-[510px] lg:w-[1320px]">
+                    <div className="w-full pt-10  lg:flex-none lg:w-[990px] lg:h-[450px] ">
+                        <div className="bg-white rounded-2xl p-10  lg:w-[966px] lg:h-[450px]">
                             <form id="contact" onSubmit={handleSubmit}>
                                 <h2 className="uppercase text-gray-800 border-b border-gray-200 mb-10 pb-5 text-lg font-bold">
                                     Let's get in touch
@@ -68,7 +68,7 @@ const ContactUs = () => {
                                         {responseMessage}
                                     </div>
                                 )}
-                                <div className="flex flex-wrap -mx-2">
+                                <div className="flex flex-wrap ">
                                     <div className="w-full lg:w-1/3 px-2 mb-6">
                                         <input
                                             name="name"
@@ -105,7 +105,7 @@ const ContactUs = () => {
                                             className="w-full h-10 rounded-2xl bg-gray-100 text-gray-600 text-sm font-medium px-4 py-2 focus:outline-none mb-6"
                                         />
                                     </div>
-                                    <div className="w-full px-2 mb-6">
+                                    <div className="w-full px-2 mb-6 lg:h-[140px] lg:w-[886px]">
                                         <textarea
                                             name="message"
                                             id="message"
@@ -116,7 +116,7 @@ const ContactUs = () => {
                                             className="w-full h-36 rounded-2xl bg-gray-100 text-gray-600 text-sm font-medium px-4 py-4 focus:outline-none mb-6"
                                         ></textarea>
                                     </div>
-                                    <div className="w-full px-2">
+                                    <div className="w-full px-2 mb-6">
                                         <button
                                             type="submit"
                                             id="form-submit"
@@ -129,8 +129,8 @@ const ContactUs = () => {
                             </form>
                         </div>
                     </div>
-                    <div className="w-full lg:w-3/12 mt-10 lg:mt-0">
-                        <div className="bg-red-700 rounded-2xl p-10 text-white">
+                    <div className="w-full mt-3 lg:h-[510px] lg:w-[330px] ">
+                        <div className="bg-[#A12C2F] p-10 text-white rounded-[20px] lg:h-[510px] lg:w-[306px]">
                             <ul>
                                 <li className="mb-6 border-b border-opacity-25 pb-6 last:mb-0 last:border-none last:pb-0">
                                     <h6 className="text-sm font-semibold mb-2">Phone Number</h6>
