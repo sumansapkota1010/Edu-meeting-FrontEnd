80b15c7815ec65174f37abf96369bd1f0b724600
suman <sumansapkota777@gmail.com>
2024-09-17 19:21:33 +0545

    protectedROutefixed

--- src/pages/ProtectedRoute.jsx
+++ src/pages/ProtectedRoute.jsx
@@ -1,39 +1,35 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { fetchProfile } from '../../store/authSlice'
-import { useLocation, Navigate } from 'react-router-dom'
-import accessDeny from '../assets/accessDenied.jpg'
-
-
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchProfile } from '../../store/authSlice';
+import accessDeny from '../assets/accessDenied.jpg';
 
 const ProtectedRoute = ({ children }) => {
-    const dispatch = useDispatch()
-    const location = useLocation()
+    const dispatch = useDispatch();
+    const [profileFetched, setProfileFetched] = useState(false);
 
-    const { data: user, loading } = useSelector((state) => state.auth)
-    console.log(user)
-
+    const { data: user, loading } = useSelector((state) => state.auth);
 
     useEffect(() => {
         if (!user) {
-            dispatch(fetchProfile())
+            dispatch(fetchProfile()).then(() => setProfileFetched(true));
+        } else {
+            setProfileFetched(true);
         }
-    }, [dispatch, user])
+    }, [dispatch, user]);
 
-    if (loading) {
-        return <div>Loading...</div>
+    if (loading || !profileFetched) {
+        return <div>Loading...</div>;
     }
 
-    const isAdmin = user?.role === "admin"
+    const isAdmin = user?.role === "admin";
 
-
-    return isAdmin ? children :
-
+    return isAdmin ? children : (
         <div className="flex items-center justify-center h-screen overflow-hidden">
             <div className="h-full w-full">
-                <img className="h-full w-full object-cover animate-fadeIn" src={accessDeny} alt="Access Deny" />
+                <img className="h-full w-full object-cover animate-fadeIn" src={accessDeny} alt="Access Denied" />
             </div>
         </div>
-}
+    );
+};
 
-export default ProtectedRoute
+export default ProtectedRoute;
