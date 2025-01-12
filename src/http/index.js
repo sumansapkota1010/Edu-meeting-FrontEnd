4e433fc20e4b2e8abcc3e4106955bc5d9c0aad58
suman <sumansapkota777@gmail.com>
2025-01-12 13:39:54 +0545

    f:baseURL

--- src/http/index.js
+++ src/http/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const APIAuthenticated = (token) => {
   return axios.create({
-    baseURL: "https://edu-meeting-backend.vercel.app/admin",
+    baseURL: "https://edu-meeting-backend.onrender.com/",
     headers: {
       Authorization: `Bearer ${token}`,
     },
